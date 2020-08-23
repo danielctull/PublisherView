c50107c38b8e0a7b25f7276993e65d662b5feb5c
Daniel Tull <dt@danieltull.co.uk>
2020-08-23 21:47:31 +0100

    Update macOS version to 11

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "PublisherView",
     platforms: [
         .iOS(.v14),
-        .macOS(.v10_16),
+        .macOS(.v11),
         .tvOS(.v14),
         .watchOS(.v7)
     ],
